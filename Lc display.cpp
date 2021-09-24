92f38774ff18ab2e461589d392472bf43d753a4e
johirul398 <mdjohirulislam545@gmail.com>
2021-09-24 21:23:01 +0600

    source code

---
+++ Lc display.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f;
+   cout<<" -"<<endl;
+   cout<<"| |"<<endl;
+   cout<<" - "<<endl;
+   cout<<"  |"<<endl;
+   cout<<" - "<<endl;
+
+   cout<<" -"<<endl;
+   cout<<"| |"<<endl;
+   cout<<" - "<<endl;
+   cout<<"| |"<<endl;
+   cout<<" -"<<endl;
+
+    cout<<"-"<<endl;
+    cout<<" |"<<endl;
+    cout<<"  "<<endl;
+    cout<<" |"<<endl;
+
+
+
+
+/* -   -   -   -   -       -   -
+| | | |   | |   |   | |   |   |
+ -   -       -   -   -   -   -
+  | | |   | | |   |   |   | |
+ -   -       -   -       -   - */
+
+
+
+    return 0;
+}
