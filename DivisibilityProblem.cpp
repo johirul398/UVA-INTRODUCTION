a95d07f853234d87bae7252b3a2fd77971fc438d
johirul398 <mdjohirulislam545@gmail.com>
2021-10-07 00:17:48 +0600

    source code

---
+++ DivisibilityProblem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a,b,c,d,e,r,f,h,t,i,j;
+    cin>>e;
+    while(e--)
+    {
+        cin>>a>>b;
+        //c=0;
+        if(a%b==0)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
